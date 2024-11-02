2f3071b1a7e1633c650835a55a9ae07cdb131d10
nafargi <nafargidamena@gmail.com>
2024-11-03 01:19:49 +0300

    3 functional components were added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <WelcomeMessage /> 
+       <Header />
+      <MainContent />
+      <WelcomeMessage /> 
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,7 +35,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
+
   )
 }
 
