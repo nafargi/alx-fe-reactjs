fef1ca9fa82d57ac11403ca4c67c5932f456d3d4
nafargi <nafargidamena@gmail.com>
2024-11-03 01:48:24 +0300

    Add UserProfile component with props for name, age, and bio

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,6 +14,7 @@ function App() {
     <>
        <Header />
       <MainContent />
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
       <WelcomeMessage /> 
       <div>
         <a href="https://vite.dev" target="_blank">
