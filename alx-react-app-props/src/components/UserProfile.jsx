14b1620772e4a3719207ece6cfebc8bf50325a55
nafargi <nafargidamena@gmail.com>
2024-11-09 22:03:19 +0300

    Refactor UserProfile to consume user data from UserContext using useContext hook

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react'
-
+import UserContext from './UserContext';
 const UserProfile = (props) => {
+  const userData = React.useContext(UserContext);
     return (
       <div>
-        <h2>{props.name}</h2>
+        <h2>{props.userData.name}</h2>
         <p>Age: {props.age}</p>
         <p>Bio: {props.bio}</p>
       </div>
