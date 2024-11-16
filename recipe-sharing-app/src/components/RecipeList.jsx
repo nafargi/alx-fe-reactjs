6b154383b286c9530a66e253424e8cccfb107c9f
nafargi <nafargidamena@gmail.com>
2024-11-17 02:45:50 +0300

    Link added

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,23 @@
-  // RecipeList component
-  import { useRecipeStore } from './recipeStore';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
-    return (
-      <div>
-        {filteredRecipes.map((recipe, index) => (
-          <div key={recipe.id} style={{ marginBottom: '10px' }}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {filteredRecipes.map((recipe) => (
+        <div key={recipe.id} style={{ marginBottom: '10px' }}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <Link to={`/recipes/${recipe.id}`} style={{ color: 'blue', textDecoration: 'underline' }}>
+            View Details
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-  export default RecipeList;
+export default RecipeList;
