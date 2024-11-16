4b969a2f519d86d027c836e0f0b014f331cd94c7
nafargi <nafargidamena@gmail.com>
2024-11-17 01:46:26 +0300

    initial commit '

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import RecipetList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <RecipetList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
