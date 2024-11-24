4ea73918063d8c1c4dd3d7fcda846fb9bf55c716
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-11-24 21:45:39 +0300

    formatting the code

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import TodoList from "./TodoList";
-
 describe("TodoList Component", () => {
   test("renders initial todos", () => {
     render(<TodoList />);
