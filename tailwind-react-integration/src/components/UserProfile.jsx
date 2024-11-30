a24263043be67310b9956ea6306b05750a8c0471
nafargi <nafargidamena@gmail.com>
2024-11-30 21:47:11 +0300

    Add centered layout, rounded border, and shadow to card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 p-8 max-w-sm ">
+      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg ">
         <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
         <h1 className='text-xl text-blue-800 my-4'>John Doe</h1>
         <p className='text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
