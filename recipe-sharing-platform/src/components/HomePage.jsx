2822c63f8203a0bc9cb3abf4d349a58d7b47b3c9
nafargi <nafargidamena@gmail.com>
2024-12-07 00:03:16 +0300

    Create the Recipe Detail Component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
 import recipeData from "../data.json";
+
 
 
 function HomePage() {
@@ -9,14 +11,16 @@ function HomePage() {
         setRecipes(recipeData);
     }, []);
   return (
+
     <div className='flex justify-center '>  
         <div className='grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3'>
             {recipes.map(recipe => (
                 <div key={recipe.id} className='m-4 shadow-xl hover:shadow-2xl transition-shadow rounded'>
-                    <h2 className="text-2xl font-bold">{recipe.title}</h2>
-                    <img src={recipe.image} alt={recipe.title} width='200' />
-                    <p>{recipe.summary}</p>
-                    <button onClick={() => alert(`You clicked on ${recipe.title}`)}>More Details</button>
+                     <Link to={`/recipe/${recipe.id}`} style={{ textDecoration: "none" }}>
+                        <img src={recipe.image} alt={recipe.title} />
+                        <h2>{recipe.title}</h2>
+                        <p>{recipe.summary}</p>
+                    </Link>
                 </div>
             ))}
         </div>
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,43 @@
+import React ,{useState, useEffect}from 'react'
+import { useParams } from 'react-router-dom'
+import recipeData from '../data.json'
+
+function RecipeDetail() {
+    const {id} = useParams();
+    const [recipe, setRecipe] = useState(null);
+    useEffect(() => {
+        const recipe = recipeData.find(recipe => recipe.id === parseInt(id));
+        setRecipe(recipe);
+    }, [id]);
+    if (!recipe) return <h1>Laoding recipe details.....</h1>
+  return (
+    <div className='bg-indigo-400 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+     <section className='bg-blue-300'>
+        <h1 className='text-2xl font-bold'>{recipe.title}</h1>
+        <img src={recipe.image} alt={recipe.title} />
+        <p><strong>Summary:</strong> {recipe.summary}</p>
+     </section>
+    <section className='bg-green-300'>
+         <p><strong className='text-2xl font-bold'>Ingredients:</strong></p>
+            <ul>
+                {/* Assuming the JSON has a field 'ingredients' that is an array */}
+                {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+                ))}
+            </ul>
+    </section>
+      <section className='bg-red-300'>
+            <p><strong className='text-2xl font-bold'>Cooking Steps:</strong></p>
+            <ol>
+                {/* Assuming the JSON has a field 'steps' that is an array */}
+                {recipe.steps && recipe.steps.map((step, index) => (
+                <li key={index}>{step}</li>
+                ))}
+            </ol>
+      </section>
+      
+    </div>
+  )
+}
+
+export default RecipeDetail
