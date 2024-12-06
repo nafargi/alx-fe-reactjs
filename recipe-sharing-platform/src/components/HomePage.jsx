513aad5ea204b3f8f7ce42f3e8cf322f6bcf6833
nafargi <nafargidamena@gmail.com>
2024-12-06 23:12:34 +0300

    Building the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,8 +12,8 @@ function HomePage() {
     <div className='flex justify-center '>  
         <div className='flex md:flex-row md:justify-center flex-col'>
             {recipes.map(recipe => (
-                <div key={recipe.id} className='m-4'>
-                    <h2>{recipe.title}</h2>
+                <div key={recipe.id} className='m-4 shadow-xl hover:shadow-2xl transition-shadow rounded'>
+                    <h2 className="text-2xl font-bold">{recipe.title}</h2>
                     <img src={recipe.image} alt={recipe.title} width='200' />
                     <p>{recipe.summary}</p>
                     <button onClick={() => alert(`You clicked on ${recipe.title}`)}>More Details</button>
