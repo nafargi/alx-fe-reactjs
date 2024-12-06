ebdf556b7565f5b238f01839e03364d2befbe390
nafargi <nafargidamena@gmail.com>
2024-12-06 22:14:30 +0300

    initial commit for the project

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1  className='text-2xl bg-red-500 text-yellow-500'>Hello World</h1>
+        
+    </>
+  )
+}
+
+export default App
